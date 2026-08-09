630dcf0e02bcbabb0e4dcc02dba73c73ff68570d
김경훈 <ensh12621@naver.com>
2026-08-09 20:24:01 +0900

    로그인 기능 추가

--- app/lib/member-api.ts
+++ app/lib/member-api.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function saveNewMember(email: string, password: string, nickname: string) {
@@ -29,6 +30,44 @@ export async function saveNewMember(email: string, password: string, nickname: s
             }
         });
     } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function login(email : string, password : string){
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    try{
+        const data = {
+            email: email,
+            password: password
+        };
+
+        console.log("api url => " + apiUrl);
+
+        console.log(`api login() .. ${email} / ${password}`);
+
+        const jwt = await fetch(`${apiUrl}/member/login`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then( response => response.text());
+        
+        const cookieStore = await cookies();
+        cookieStore.set('jwt', jwt, {
+            httpOnly : true,
+            secure : false, // 아직 https 인증서 없으니..
+            sameSite: "lax",
+            maxAge: 60 * 5, // 5분 유지
+            path: "/"
+        });
+
+        redirect("/");
+
+    }catch(error){
         console.log(error);
         throw error;
     }
--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -4,10 +4,40 @@ import Link from "next/link";
 import Button1 from "../ui/Button1";
 import Header from "../ui/Header"
 import Input1 from "../ui/Input1";
+import React, { useState } from "react";
+import { login } from "../lib/member-api";
 
 export default function LoginForm() {
 
     
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = ( e : React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const checkValidity = (email : string, password : string) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValidEmail = regex.test(email);
+        const isValidPassword = password.length > 6;
+
+        return isValidEmail && isValidPassword;
+    };
+
+    const handleLogin = async () => {
+        if(!checkValidity(email, password)){
+            alert("로그인 시도 시 정확한 값을 입력해주세요.");
+            return;
+        }
+
+        await login(email, password);
+    };
+
     return (
         <div className="border-1 border-gray-300 rounded-lg w-4/10 min-h-4/10 m-auto mt-30 p-15 flex flex-col gap-15">
             <Header 
@@ -18,11 +48,15 @@ export default function LoginForm() {
                 <Input1 
                     placeholder="이메일" 
                     extraCss="border-b-1 border-gray-300 text-lg"
+                    value={email}
+                    onChange={handleEmailChange}
                 />
                 <Input1 
                     placeholder="비밀번호"
                     passwordMode={true}
                     extraCss="border-b-1 border-gray-300 text-lg"
+                    value={password}
+                    onChange={handlePasswordChange}
                 />
             </div>
 
@@ -30,6 +64,7 @@ export default function LoginForm() {
                 <Button1
                     title="로그인"
                     color="blue"
+                    onClick={handleLogin}
                 />  
                 <Link 
                     href="/signup">
